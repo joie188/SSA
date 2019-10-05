c3132ee62f100a4936a356285667174839d004da
Joie Le <joie188@gmail.com>
2019-10-05 12:07:53 -0400

    added forest & reg

--- Week2/sat_classifier.py
+++ Week2/sat_classifier.py
@@ -6,9 +6,17 @@ Created on Fri Oct  4 21:10:06 2019
 """
 
 import numpy as np
-import random
 import pandas as pd
+import matplotlib.pyplot as plt
+import random
 import string
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.externals import joblib
         
 def can_be_floated(array):
     for i in array:
@@ -40,6 +48,40 @@ def all_same(array):
             return False
     return True
 
+def random_forest(data, orbit):
+    x = np.column_stack(data)[1:, 2:]       #independent variables (e, a, p, T)
+    y = np.column_stack(data)[1:, orbit]    #class or type of orbit
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 21)
+    # Feature Scaling
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 42)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+
+    print("-----RANDOM FOREST-----")
+    ct = pd.crosstab(y_test, y_pred, rownames=['Actual Orbit'], colnames=['Predicted Orbit'])
+    print(ct)
+    print('Misclassified samples: {}'.format((y_test != y_pred).sum()))
+    print(accuracy_score(y_test, y_pred))
+    print("[perigee, apogee, eccentricity, inclination, period]")
+    print(classifier.feature_importances_)
+    #ct.plot.bar(stacked=True)
+    #plt.legend(title='Orbit')
+    #plt.show()
+    print()
+    print("-----LOGISTIC REGRESSION-----")
+    model = LogisticRegression(solver = 'lbfgs', multi_class='ovr')
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    ct = pd.crosstab(y_test, y_pred, rownames=['Actual Orbit'], colnames=['Predicted Orbit'])
+    print(ct)
+    print('Misclassified samples: {}'.format((y_test != y_pred).sum()))
+    print(accuracy_score(y_test, y_pred))
+    print("[perigee, apogee, eccentricity, inclination, period]")
+    print(model.coef_)
 
 if __name__=='__main__':
 
@@ -55,11 +97,14 @@ if __name__=='__main__':
         if can_be_floated(data_list):
             org_data[attr] = floatify(data_list)
 
-    for attr in org_data:
+    '''for attr in org_data:
         print(attr, type(org_data[attr][0]), all_same(org_data[attr]))
-        print('--------------------')
+        print('--------------------')'''
     
     col_of_interest = {}
+    data_as_mat = []
     for attr, val in org_data.items():
         if 'Orbit' in attr or 'Perigee' in attr or 'Apogee' in attr or 'Eccentricity' in attr or 'Inclination' in attr or 'Period' in attr:
-            col_of_interest[attr] = val       
+            col_of_interest[attr] = val  
+            data_as_mat.append([attr] + val) 
+    random_forest(data_as_mat, 0)
