f0446ee058d9e0bfc855e99ea875b82c0c7f130f
Joie Le <joie188@gmail.com>
2019-10-05 12:09:29 -0400

    merge?

--- Week2/sat_classifier.py
+++ Week2/sat_classifier.py
@@ -48,46 +48,9 @@ def all_same(array):
             return False
     return True
 
-<<<<<<< HEAD
-def random_forest(data, orbit):
-    x = np.column_stack(data)[1:, 2:]       #independent variables (e, a, p, T)
-    y = np.column_stack(data)[1:, orbit]    #class or type of orbit
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 21)
-    # Feature Scaling
-    scaler = StandardScaler()
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.transform(X_test)
-
-    classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 42)
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_test)
-
-    print("-----RANDOM FOREST-----")
-    ct = pd.crosstab(y_test, y_pred, rownames=['Actual Orbit'], colnames=['Predicted Orbit'])
-    print(ct)
-    print('Misclassified samples: {}'.format((y_test != y_pred).sum()))
-    print(accuracy_score(y_test, y_pred))
-    print("[perigee, apogee, eccentricity, inclination, period]")
-    print(classifier.feature_importances_)
-    #ct.plot.bar(stacked=True)
-    #plt.legend(title='Orbit')
-    #plt.show()
-    print()
-    print("-----LOGISTIC REGRESSION-----")
-    model = LogisticRegression(solver = 'lbfgs', multi_class='ovr')
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    ct = pd.crosstab(y_test, y_pred, rownames=['Actual Orbit'], colnames=['Predicted Orbit'])
-    print(ct)
-    print('Misclassified samples: {}'.format((y_test != y_pred).sum()))
-    print(accuracy_score(y_test, y_pred))
-    print("[perigee, apogee, eccentricity, inclination, period]")
-    print(model.coef_)
-=======
 def is_relevant(attr):
     result = 'Perigee' in attr or 'Apogee' in attr or 'Eccentricity' in attr or 'Inclination' in attr or 'Period' in attr
     return result
->>>>>>> 2c99fde43e115ed5c7a2def8a3b8f41e90747483
 
 def read_data(data, depth):
     '''
@@ -107,19 +70,6 @@ class Satellite:
 """ABOVE ARE USEFUL FUNCTIONS"""
 
 
-<<<<<<< HEAD
-    '''for attr in org_data:
-        print(attr, type(org_data[attr][0]), all_same(org_data[attr]))
-        print('--------------------')'''
-    
-    col_of_interest = {}
-    data_as_mat = []
-    for attr, val in org_data.items():
-        if 'Orbit' in attr or 'Perigee' in attr or 'Apogee' in attr or 'Eccentricity' in attr or 'Inclination' in attr or 'Period' in attr:
-            col_of_interest[attr] = val  
-            data_as_mat.append([attr] + val) 
-    random_forest(data_as_mat, 0)
-=======
         
 data = pd.read_csv("Week2_Problem2.csv")
 org_data = {}
@@ -141,4 +91,3 @@ for i in read_data(org_data, 2062):
     
 if __name__=='__main__':
     pass
->>>>>>> 2c99fde43e115ed5c7a2def8a3b8f41e90747483
