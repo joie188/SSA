2c99fde43e115ed5c7a2def8a3b8f41e90747483
Guis99 <[bhoh@mit.edu]>
2019-10-05 12:06:20 -0400

    created satellite class

--- Week2/sat_classifier.py
+++ Week2/sat_classifier.py
@@ -40,26 +40,46 @@ def all_same(array):
             return False
     return True
 
+def is_relevant(attr):
+    result = 'Perigee' in attr or 'Apogee' in attr or 'Eccentricity' in attr or 'Inclination' in attr or 'Period' in attr
+    return result
 
-if __name__=='__main__':
+def read_data(data, depth):
+    '''
+    Parses through prepared data and outputs convenient data for Satellite class initialization
+    '''
+    for i in range(depth):
+        yield (data['Name of Satellite, Alternate Names'][i] ,{key:data[key][i] for key in data if is_relevant(key)}, data['Class of Orbit'][i], data['Type of Orbit'][i])
 
-    data = pd.read_csv("Week2_Problem2.csv")
-    org_data = {}
+class Satellite:
+    def __init__(self, interest_param):
+        self.name, self.orbit_param, self.orbit_class, self.orbit_type = interest_param
 
-    for i in data:
-        if 'Unnamed' not in i and i != 'Comments' and 'Source' not in i:
-            org_data[i] = [i for i in data[i]]
+    def __repr__(self):
+        return repr(self.name) + repr(self.orbit_class) + repr(self.orbit_type)
 
-    for attr in org_data:
-        data_list = org_data[attr]
-        if can_be_floated(data_list):
-            org_data[attr] = floatify(data_list)
 
-    for attr in org_data:
-        print(attr, type(org_data[attr][0]), all_same(org_data[attr]))
-        print('--------------------')
+"""ABOVE ARE USEFUL FUNCTIONS"""
+
+
+        
+data = pd.read_csv("Week2_Problem2.csv")
+org_data = {}
+
+for i in data:
+    if 'Unnamed' not in i and i != 'Comments' and 'Source' not in i:
+        org_data[i] = [i for i in data[i]]
+
+for attr in org_data:
+    data_list = org_data[attr]
+    if can_be_floated(data_list):
+        org_data[attr] = floatify(data_list)
+        
+sat_list = []
+  
+for i in read_data(org_data, 2062):
+    sat_list.append(Satellite(i))
+
     
-    col_of_interest = {}
-    for attr, val in org_data.items():
-        if 'Orbit' in attr or 'Perigee' in attr or 'Apogee' in attr or 'Eccentricity' in attr or 'Inclination' in attr or 'Period' in attr:
-            col_of_interest[attr] = val       
+if __name__=='__main__':
+    pass
