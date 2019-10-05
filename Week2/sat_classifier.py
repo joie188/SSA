26e95a11395179cbef5b30f82e34bd072b45eb91
Joie Le <joie188@gmail.com>
2019-10-05 10:40:24 -0400

    extract columsn for classification

--- Week2/sat_classifier.py
+++ Week2/sat_classifier.py
@@ -10,14 +10,6 @@ import random
 import pandas as pd
 import string
         
-data = pd.read_csv("Week2_Problem2.csv")
-
-org_data = {}
-
-for i in data:
-    if 'Unnamed' not in i and i != 'Comments' and 'Source' not in i:
-        org_data[i] = [i for i in data[i]]
-
 def can_be_floated(array):
     for i in array:
         if isinstance(i, float) or isinstance(i, int):
@@ -37,15 +29,10 @@ def floatify(array):
                 i = float(i.translate(str.maketrans('', '', string.punctuation)))
             except ValueError:
                 pass
-        
         new_array.append(i)   
     return new_array
 
-for attr in org_data:
-    data_list = org_data[attr]
-    if can_be_floated(data_list):
-        org_data[attr] = floatify(data_list)
-        
+
 def all_same(array):
     same_type = type(array[0])
     for i in array:
@@ -53,6 +40,26 @@ def all_same(array):
             return False
     return True
 
-for attr in org_data:
-    print(attr, type(org_data[attr][0]), all_same(org_data[attr]))
-    print('--------------------')
+
+if __name__=='__main__':
+
+    data = pd.read_csv("Week2_Problem2.csv")
+    org_data = {}
+
+    for i in data:
+        if 'Unnamed' not in i and i != 'Comments' and 'Source' not in i:
+            org_data[i] = [i for i in data[i]]
+
+    for attr in org_data:
+        data_list = org_data[attr]
+        if can_be_floated(data_list):
+            org_data[attr] = floatify(data_list)
+
+    for attr in org_data:
+        print(attr, type(org_data[attr][0]), all_same(org_data[attr]))
+        print('--------------------')
+    
+    col_of_interest = {}
+    for attr, val in org_data.items():
+        if 'Orbit' in attr or 'Perigee' in attr or 'Apogee' in attr or 'Eccentricity' in attr or 'Inclination' in attr or 'Period' in attr:
+            col_of_interest[attr] = val       
