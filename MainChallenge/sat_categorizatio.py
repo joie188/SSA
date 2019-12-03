a37c485e093ce6bac9f12c3fca14dc459224c6f6
Brian Hoh <bhoh@mit.edu>
2019-12-03 16:56:48 -0500

    changed data processing

---
+++ MainChallenge/sat_categorizatio.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 16:42:40 2019
+
+@author: Brian
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+from sat_data import gather_data
+
+data = gather_data("AstroYWeek3DataSet.csv", 2)
+
--- MainChallenge/sat_data.py
+++ MainChallenge/sat_data.py
@@ -5,108 +5,116 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
-sat_dict = {}
+#%%
 
 data = pd.read_csv("AstroYWeek1DataSet.csv")
 
 data = pd.read_csv("AstroYWeek2DataSeT.csv")
 
-current_id = 0
+def gather_data(file, pattern_type):
+    data = pd.read_csv(file)
+    current_id = 0
+    sat_dict = {}
+    epoch_time = []
     
-epoch_time = []
-for i in range(len(data)):
-    row = data.iloc[i]
-    pattern = '%Y-%m-%d %H:%M:%S'
-    #pattern = '%m/%d/%Y %H:%M'
-    norm_time = int(time.mktime(time.strptime(row["EPOCH"], pattern)))
-    epoch_time.append(norm_time)
+    for i in range(len(data)):
+        row = data.iloc[i]
+        if pattern_type == 1:
+            pattern = '%Y-%m-%d %H:%M:%S'
+        else:
+            pattern = '%m/%d/%Y %H:%M'
+        norm_time = int(time.mktime(time.strptime(row["EPOCH"], pattern)))
+        epoch_time.append(norm_time)
+    
+    data['TIME'] = epoch_time
 
-data['TIME'] = epoch_time
+    for i in range(len(data)):
+        row = data.iloc[i]
+        if row["NORAD_CAT_ID"] != current_id:
+            current_id = row['NORAD_CAT_ID']
+            sat_dict[current_id] = data.loc[data.NORAD_CAT_ID == current_id]
+            
+    return sat_dict
 
 #%%
-for i in range(len(data)):
-    row = data.iloc[i]
-    if row["NORAD_CAT_ID"] != current_id:
-        current_id = row['NORAD_CAT_ID']
-        sat_dict[current_id] = data.loc[data.NORAD_CAT_ID == current_id]
-
-#%%
-sat_09191 = sat_dict[9191]
-sat_11682 = sat_dict[11682]
-sat_30206 = sat_dict[30206]
-sat_79815 = sat_dict[79815]
-
-plt.subplot(5, 2, 1)
-plt.plot(sat_09191.TIME, sat_09191.MEAN_MOTION)
-plt.plot(sat_11682.TIME, sat_11682.MEAN_MOTION)
-plt.plot(sat_30206.TIME, sat_30206.MEAN_MOTION)
-plt.plot(sat_79815.TIME, sat_79815.MEAN_MOTION)
-plt.title("mean motion")
-
-plt.subplot(5,2,2)
-plt.plot(sat_09191.TIME,sat_09191.ECCENTRICITY)
-plt.plot(sat_11682.TIME,sat_11682.ECCENTRICITY)
-plt.plot(sat_30206.TIME,sat_30206.ECCENTRICITY)
-plt.plot(sat_79815.TIME,sat_79815.ECCENTRICITY)
-plt.title("eccentricity")
-
-plt.subplot(5,2,3)
-plt.plot(sat_09191.TIME,sat_09191.INCLINATION)
-plt.plot(sat_11682.TIME,sat_11682.INCLINATION)
-plt.plot(sat_30206.TIME,sat_30206.INCLINATION)
-plt.plot(sat_79815.TIME,sat_79815.INCLINATION)
-plt.title('inclination')
-
-plt.subplot(5,2,4)
-plt.plot(sat_09191.TIME,sat_09191.RA_OF_ASC_NODE)
-plt.plot(sat_11682.TIME,sat_11682.RA_OF_ASC_NODE)
-plt.plot(sat_30206.TIME,sat_30206.RA_OF_ASC_NODE)
-plt.plot(sat_79815.TIME,sat_79815.RA_OF_ASC_NODE)
-plt.title('ra of asc node')
-
-plt.subplot(5,2,5)
-plt.plot(sat_09191.TIME,sat_09191.ARG_OF_PERICENTER)
-plt.plot(sat_11682.TIME,sat_11682.ARG_OF_PERICENTER)
-plt.plot(sat_30206.TIME,sat_30206.ARG_OF_PERICENTER)
-plt.plot(sat_79815.TIME,sat_79815.ARG_OF_PERICENTER)
-plt.title('pericenter')
-
-plt.subplot(5,2,6)
-plt.plot(sat_09191.TIME,sat_09191.MEAN_ANOMALY)
-plt.plot(sat_11682.TIME,sat_11682.MEAN_ANOMALY)
-plt.plot(sat_30206.TIME,sat_30206.MEAN_ANOMALY)
-plt.plot(sat_79815.TIME,sat_79815.MEAN_ANOMALY)
-plt.title('mean anomaly')
-
-plt.subplot(5,2,7)
-plt.plot(sat_09191.TIME,sat_09191.SEMIMAJOR_AXIS)
-plt.plot(sat_11682.TIME,sat_11682.SEMIMAJOR_AXIS)
-plt.plot(sat_30206.TIME,sat_30206.SEMIMAJOR_AXIS)
-plt.plot(sat_79815.TIME,sat_79815.SEMIMAJOR_AXIS)
-plt.title('semimajor axis')
-
-plt.subplot(5,2,8)
-plt.plot(sat_09191.TIME,sat_09191.PERIOD)
-plt.plot(sat_11682.TIME,sat_11682.PERIOD)
-plt.plot(sat_30206.TIME,sat_30206.PERIOD)
-plt.plot(sat_79815.TIME,sat_79815.PERIOD)
-plt.title('period')
-
-plt.subplot(5,2,9)
-plt.plot(sat_09191.TIME,sat_09191.APOGEE)
-plt.plot(sat_11682.TIME,sat_11682.APOGEE)
-plt.plot(sat_30206.TIME,sat_30206.APOGEE)
-plt.plot(sat_79815.TIME,sat_79815.APOGEE)
-plt.title('apogee')
-
-plt.subplot(5,2,10)
-plt.plot(sat_09191.TIME,sat_09191.PERIGEE)
-plt.plot(sat_11682.TIME,sat_11682.PERIGEE)
-plt.plot(sat_30206.TIME,sat_30206.PERIGEE)
-plt.plot(sat_79815.TIME,sat_79815.PERIGEE)
-plt.title('perigee')
-
-plt.show()
+        
+#sat_dict = gather_data("AstroYWeek2DataSeT.csv", 1)
+#sat_09191 = sat_dict[9191]
+#sat_11682 = sat_dict[11682]
+#sat_30206 = sat_dict[30206]
+#sat_79815 = sat_dict[79815]
+#
+#plt.subplot(5, 2, 1)
+#plt.plot(sat_09191.TIME, sat_09191.MEAN_MOTION)
+#plt.plot(sat_11682.TIME, sat_11682.MEAN_MOTION)
+#plt.plot(sat_30206.TIME, sat_30206.MEAN_MOTION)
+#plt.plot(sat_79815.TIME, sat_79815.MEAN_MOTION)
+#plt.title("mean motion")
+#
+#plt.subplot(5,2,2)
+#plt.plot(sat_09191.TIME,sat_09191.ECCENTRICITY)
+#plt.plot(sat_11682.TIME,sat_11682.ECCENTRICITY)
+#plt.plot(sat_30206.TIME,sat_30206.ECCENTRICITY)
+#plt.plot(sat_79815.TIME,sat_79815.ECCENTRICITY)
+#plt.title("eccentricity")
+#
+#plt.subplot(5,2,3)
+#plt.plot(sat_09191.TIME,sat_09191.INCLINATION)
+#plt.plot(sat_11682.TIME,sat_11682.INCLINATION)
+#plt.plot(sat_30206.TIME,sat_30206.INCLINATION)
+#plt.plot(sat_79815.TIME,sat_79815.INCLINATION)
+#plt.title('inclination')
+#
+#plt.subplot(5,2,4)
+#plt.plot(sat_09191.TIME,sat_09191.RA_OF_ASC_NODE)
+#plt.plot(sat_11682.TIME,sat_11682.RA_OF_ASC_NODE)
+#plt.plot(sat_30206.TIME,sat_30206.RA_OF_ASC_NODE)
+#plt.plot(sat_79815.TIME,sat_79815.RA_OF_ASC_NODE)
+#plt.title('ra of asc node')
+#
+#plt.subplot(5,2,5)
+#plt.plot(sat_09191.TIME,sat_09191.ARG_OF_PERICENTER)
+#plt.plot(sat_11682.TIME,sat_11682.ARG_OF_PERICENTER)
+#plt.plot(sat_30206.TIME,sat_30206.ARG_OF_PERICENTER)
+#plt.plot(sat_79815.TIME,sat_79815.ARG_OF_PERICENTER)
+#plt.title('pericenter')
+#
+#plt.subplot(5,2,6)
+#plt.plot(sat_09191.TIME,sat_09191.MEAN_ANOMALY)
+#plt.plot(sat_11682.TIME,sat_11682.MEAN_ANOMALY)
+#plt.plot(sat_30206.TIME,sat_30206.MEAN_ANOMALY)
+#plt.plot(sat_79815.TIME,sat_79815.MEAN_ANOMALY)
+#plt.title('mean anomaly')
+#
+#plt.subplot(5,2,7)
+#plt.plot(sat_09191.TIME,sat_09191.SEMIMAJOR_AXIS)
+#plt.plot(sat_11682.TIME,sat_11682.SEMIMAJOR_AXIS)
+#plt.plot(sat_30206.TIME,sat_30206.SEMIMAJOR_AXIS)
+#plt.plot(sat_79815.TIME,sat_79815.SEMIMAJOR_AXIS)
+#plt.title('semimajor axis')
+#
+#plt.subplot(5,2,8)
+#plt.plot(sat_09191.TIME,sat_09191.PERIOD)
+#plt.plot(sat_11682.TIME,sat_11682.PERIOD)
+#plt.plot(sat_30206.TIME,sat_30206.PERIOD)
+#plt.plot(sat_79815.TIME,sat_79815.PERIOD)
+#plt.title('period')
+#
+#plt.subplot(5,2,9)
+#plt.plot(sat_09191.TIME,sat_09191.APOGEE)
+#plt.plot(sat_11682.TIME,sat_11682.APOGEE)
+#plt.plot(sat_30206.TIME,sat_30206.APOGEE)
+#plt.plot(sat_79815.TIME,sat_79815.APOGEE)
+#plt.title('apogee')
+#
+#plt.subplot(5,2,10)
+#plt.plot(sat_09191.TIME,sat_09191.PERIGEE)
+#plt.plot(sat_11682.TIME,sat_11682.PERIGEE)
+#plt.plot(sat_30206.TIME,sat_30206.PERIGEE)
+#plt.plot(sat_79815.TIME,sat_79815.PERIGEE)
+#plt.title('perigee')
+#
+#plt.show()
 
      
 
